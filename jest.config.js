939505dc84150e0b103252875c065251070c2f23
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-17 22:39:52 +0700

    refactor: init test

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/**
+ * @type {import('jest').Config}
+ */
+module.exports = {
+	// It indicates the listing where Jest must save the cached dependency details gathered from all throughout the tests
+	cacheDirectory: "./tmp/jest",
+};
